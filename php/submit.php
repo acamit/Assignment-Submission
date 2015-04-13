9923a88ae3820c47a1867312cf93df126737b926
Himanshu Gupta <himanshg12@gmail.com>
2015-04-13 23:04:36 +0530

    student home page added

--- php/submit.php
+++ php/submit.php
@@ -112,7 +112,29 @@ else{
 		<script src= "../js/submission.js" type = "text/javascript"></script>
 	</head>
 	
-	<body>
+	<body><br/>
+		<div class="ui secondary pointing  menu">
+		  <a class="item" href="student_home.php">
+			<i class="home icon" ></i> Home
+		  </a>
+		  <a class=" active item" href = "submit.php">
+			<i class="upload icon"></i> Submit Assignment
+		  </a>
+		  <!--<a class="item" href="" >
+			<i class = "download icon"></i> Check Marks
+		  </a>-->
+		  <div class="right menu">
+			<a class="ui item">
+				<i class = "user icon"></i><?php echo $_SESSION['name']; ?> 
+			</a>
+			<a class="ui item" href="changePassword.php">
+				<i class = "server icon"></i>Change Password
+			</a>
+			<a class="ui item" href="logout.php">
+			  <i class = "power icon"></i>Logout
+			</a>
+			</div>
+		</div>
 		<div class = "header">
 			<h1 class = "ui black header">Department of Computer Science And Engineering</h1>
 			<h2 class ="ui green header">Assignment Submission Portal</h2>
