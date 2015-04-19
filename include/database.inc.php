517e1116c813cf47faebe2eb9ff2cfeb2ae164a3
Amit Chawla <acamit84@gmail.com>
2015-04-19 16:32:15 +0530

    table queries

--- include/database.inc.php
+++ include/database.inc.php
@@ -1,11 +1,14 @@
 <?php
 	
-	$host = 'localhost';
+	$host = '192.163.240.195';
+	$user = 'tssgndu_asg';
+	$pass = 'KM9Q9fO2P?zh';
+	$database = 'tssgndu_asg' ;
+	/*$host = 'localhost';
 	$user = 'root';
 	$pass = '';
-	$database = 'assignment' ;
+	$database = 'tssgndu_asg' ;*/
 	if(!mysql_connect($host , $user , $pass) || !mysql_select_db($database)){
 		die(mysql_error());
-		
 	}
 ?>
